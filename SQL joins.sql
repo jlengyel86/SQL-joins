2fc3e2df00ce5574d220931c1e75ac0240afe69b
Jamie Lengyel <133892004+jlengyel86@users.noreply.github.com>
2024-12-11 12:19:18 -0600

    Add files via upload

--- SQL joins.sql
+++ SQL joins.sql
@@ -36,3 +36,25 @@ ON e.EmployeeID = s.EmployeeID
 GROUP BY e.EmployeeID
 ORDER BY Total Desc
 Limit 5;
+
+SELECT d.Name AS Department, c.Name AS Category FROM categories AS c
+INNER JOIN departments AS d
+ON d.DepartmentID = c.DepartmentID
+WHERE c.Name = "Appliences" OR c.Name = "Games";
+
+SELECT p.Name AS product
+SUM(s.Quantity) AS TotalQuantity
+SUM(s.PricePoint * s.Quantity) AS TotalValue
+FROM products AS p
+INNER JOIN sales AS s
+ON p.ProductID = s.ProductID
+WHERE p.Name = "Eagles: Hotal California";
+
+SELECT p.Name AS product, r.Reviewer, r.Rating, r.Comment
+FROM revies AS r
+INNER JOIN products AS p
+ON p.ProductID = r.ProductID
+WHERE p.Name = "VisioTV"
+ORDER BY r.Rating
+LIMIT 1;
+
